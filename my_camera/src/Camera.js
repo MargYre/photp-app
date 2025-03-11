73740df7ee44aa63ff717258a03950a1f902d0ca
Margaux <margaux.tarayre@gmail.com>
2025-03-11 14:53:50 +0100

    Add camera photo capture (takePicture)

--- my_camera/src/Camera.js
+++ my_camera/src/Camera.js
@@ -1,11 +1,15 @@
-import React, { useState } from 'react';
-import { Button, Text, TouchableOpacity, View } from 'react-native';
+import React, { useState, useRef } from 'react';
+import { Button, Text, TouchableOpacity, View, Image } from 'react-native';
 import { CameraView, useCameraPermissions } from 'expo-camera';
+//import * as FileSystem from 'expo-file-system';
 import styles from './styles/styles';
 
 const Camera = () => {
   const [facing, setFacing] = useState('back');
   const [permission, requestPermission] = useCameraPermissions();
+  const [previewVisible, setPreviewVisible] = useState(false);
+  const [capturedImage, setCapturedImage] = useState(null);
+  const cameraRef = useRef(null);
   
   if (!permission) {
     return <View />;
@@ -24,15 +28,60 @@ const Camera = () => {
     setFacing(current => (current === 'back' ? 'front' : 'back'));
   };
   
+  const takePicture = async () => {
+    if (!cameraRef.current) return;
+    try {
+      const photo = await cameraRef.current.takePictureAsync();
+      console.log(photo);
+      setPreviewVisible(true);
+      setCapturedImage(photo);
+
+      //Utiliser expo-file-system' pour save de façon permanente
+
+    } catch (error) {
+      console.error("Error taking picture:", error);
+    }
+  };
+
+  const __retakePicture = () => {
+    setCapturedImage(null);
+    setPreviewVisible(false);
+  };
+
   return (
     <View style={styles.container}>
-      <CameraView style={styles.camera} facing={facing}>
-        <View style={styles.buttonContainer}>
-          <TouchableOpacity style={styles.button} onPress={toggleCameraFacing}>
-            <Text style={styles.text}>Flip Camera</Text>
-          </TouchableOpacity>
+      {previewVisible && capturedImage ? (
+        <View style={styles.container}>
+          <Image
+            source={{ uri: capturedImage.uri }}
+            style={styles.camera}
+          />
+          <View style={styles.buttonContainer}>
+            <TouchableOpacity style={styles.button} onPress={__retakePicture}>
+              <Text style={styles.text}>Retake</Text>
+            </TouchableOpacity>
+          </View>
         </View>
-      </CameraView>
+      ) : (
+        <CameraView
+          style={styles.camera}
+          facing={facing}
+          ref={cameraRef}
+        >
+          <View style={styles.buttonContainer}>
+            <TouchableOpacity style={styles.button} onPress={toggleCameraFacing}>
+              <Text style={styles.text}>Flip Camera</Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity
+              onPress={takePicture}
+              style={styles.photoButton}
+            >
+              <Text style={styles.text}>PHOTO</Text>
+            </TouchableOpacity>
+          </View>
+        </CameraView>
+      )}
     </View>
   );
 };
